1a193edf807b2753bab60dd902bb787bdf47f61d
TJQKAs <a26052013@gmail.com>
2015-09-03 15:16:45 +0000

    update

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,10 @@
 require 'docking_station'
 describe DockingStation do
   it {is_expected.to respond_to :release_bike}
-end
+  
+  it 'gets working bike' do 
+    bike = subject.release_bike
+   # expect(:release_bike) == (Bike.new) 
+    expect(bike).to be_working
+  end
+  end
